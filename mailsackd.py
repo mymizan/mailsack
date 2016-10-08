01f2a1c33359244b5dc79213bde6011033e640f0
M Yakub Mizan <ctgcoder@gmail.com>
2016-10-08 19:41:49 +0600

    updated listening port for clients

--- mailsackd.py
+++ mailsackd.py
@@ -26,15 +26,13 @@ class Mailsack_Server(smtpd.SMTPServer):
         self.save_message(mail)
         self.print_message(mail)
 
-    def client_listener():
-        pass
-
 if __name__ == '__main__':
     #get default arguments
     try:
         parser = argparse.ArgumentParser(description='')
         parser.add_argument('--host', default='localhost', help='local IP address to listen to')
         parser.add_argument('--port', default='25', help='port to listen')
+        parser.add_argument('--listen', default='None', help='listen for client connections')
 
         args = parser.parse_args()
         args = vars(args)
@@ -47,6 +45,14 @@ if __name__ == '__main__':
     except(PermissionError):
         print("Can not bind to port " + args['port'] + ". Please, run as root")
         sys.exit()
+
+    # listen for incoming connection from GUI
+    if args['listen'] != 'None':
+        try:
+            sockserver = SockServer(args['host'], int(args['listen']))
+            sockserver.listen()
+        except:
+            print("Can not start socket server. Incoming connections from the UI will be denied.")
 
     try:
         asyncore.loop()
